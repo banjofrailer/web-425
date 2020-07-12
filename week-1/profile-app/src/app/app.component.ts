f1aeaf88ed0bf7e6c65cde2c96925c389eeee87a
Sarah Kovar <sarahkovar@gmail.com>
2020-07-12 18:59:27 -0500

    week 1

---
+++ week-1/profile-app/src/app/app.component.ts
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title:  app.component.ts
+; Author: Professor Krasso
+; Date:   12 July 2020
+; Modified by: Sarah Kovar
+; Description: App component
+;===========================================
+*/
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment: string = 'Assignment 1.5 - Components';
+}
---
+++ week-1/profile-app/src/app/my-details/my-details.component.ts
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title:  my-details.component.ts
+; Author: Professor Krasso
+; Date:   12 July 2020
+; Modified by: Sarah Kovar
+; Description: My details components
+;===========================================
+*/
+
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+export class MyDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

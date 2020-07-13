2c1e73eefb850bd433d7ca5f486f41fe27684bd5
Sarah Kovar <sarahkovar@gmail.com>
2020-07-12 19:22:21 -0500

    code attribution update

--- week-1/profile-app/src/app/app-routing.module.ts
+++ week-1/profile-app/src/app/app-routing.module.ts
@@ -1,3 +1,13 @@
+/*
+============================================
+; Title:  app-routing.module.ts
+; Author: Professor Krasso
+; Date:   12 July 2020
+; Modified by: Sarah Kovar
+; Description: App routing module
+;===========================================
+*/
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 

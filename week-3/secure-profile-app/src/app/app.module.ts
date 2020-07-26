f61df03e98b982c3a434fbd565e010e50de8a374
Sarah Kovar <sarahkovar@gmail.com>
2020-07-26 12:54:55 -0500

    week 3

---
+++ week-3/secure-profile-app/src/app/app.module.ts
@@ -0,0 +1,41 @@
+/*
+============================================
+; Title:  app.module.ts
+; Author: Professor Krasso
+; Date:   12 July 2020
+; Modified by: Sarah Kovar
+; Description: App module
+;===========================================
+*/
+
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
+import { SigninComponent } from './signin/signin.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutes } from './app.routing';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyImageComponent,
+    MyDetailsComponent,
+    SigninComponent,
+    HomeComponent,
+    SigninComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

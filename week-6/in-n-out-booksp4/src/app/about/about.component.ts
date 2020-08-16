6803ac574669983d6a9e136080598a8b983f92b2
Sarah Kovar <sarahkovar@gmail.com>
2020-08-15 23:56:39 -0500

    week 6

---
+++ week-6/in-n-out-booksp4/src/app/about/about.component.ts
@@ -0,0 +1,23 @@
+/**
+ * Title: about.component.ts
+ * Author: Professor Krasso
+ * Date: 8 August 2020
+ * Modified By: Sarah Kovar
+ * Description: About component
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
